30b2f74bf568875674886f12f0c4a5a1778e7ffc
ChocoChuck <coguagha1@gmail.com>
2017-10-28 17:59:54 -0400

    Web sockets server connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 // console.log(publicPath);
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server)
 
 app.use(express.static(publicPath));
 
@@ -15,6 +18,14 @@ app.use(express.static(publicPath));
 //   res.send('hello world')
 // });
 
-app.listen(port, function(){
-  console.log(`server is running on ${port}`);
+io.on('connection', function (socket) {
+  console.log("new connection");
+
+  socket.on('disconnect', function(){
+    console.log('Disconnected');
+  })
+})
+
+server.listen(port, function(){
+  console.log(`server is running on port ${port}`);
 });
